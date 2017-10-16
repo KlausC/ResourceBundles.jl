4e226e0c005b87c607f491b83d3c5c60222709cd
Klaus Crusius <klaus.crusius@web.de>
2017-10-16 18:37:12 +0200

    tr_str macro implemented

--- src/string.jl
+++ src/string.jl
@@ -1,5 +1,4 @@
 
-
 function stringeval(ex::Expr)
     if ex.head === :string
         io = IOBuffer()
@@ -49,58 +48,53 @@ macro loc_str(p)
     :(stringevalloc(parse(string('"', $p, '"'))))
 end
 
-function string2ex(p::AbstractString)
+function string2ex(p::AbstractString, oldargs = nothing)
     ex = parse(string('"', p, '"'))
     args = isa(ex, Expr) ? ex.args : [ex]
     io = IOBuffer()
     i = 0
     ea = []
+    n = oldargs == nothing ? 0 : length(oldargs)
     for j in 1:length(args)
         a = args[j]
         if isa(a, String)
             print(io, a)
         elseif isa(a, Int)
             print(io, "\$(", a, ")")
-            args[j] = a
+            args[j] = 0 < a <= n ? oldargs[a] : a
             push!(ea, a)
         else
             i += 1
             print(io, "\$(", i, ")")
             args[j] = i
-            push!(ea, eval(a))
+            push!(ea, a)
         end
     end
     ex, ea, String(take!(io))
 end
 
-function eval_string(ex::Expr, arg::Vector)
-    io = IOBuffer()
-    for a in ex.args
-        if isa(a, Int)
-            print(io, string(arg[a]))
-        else
-            print(io, a)
-        end
-    end
-    String(take!(io))
-end
-eval_string(ex::AbstractString, arg::Vector) = ex
-
 function translate(p::AbstractString, locale::Any)
     ex1, arg, s1 = string2ex(p)
+    translate2(s1, locale, ex1, arg)
+end
+
+function translate2(s1::AbstractString, locale::Any, ex1::Any, arg)    
     s2 = lookup(locale, s1)
-    ex2, ind2, dummy2 = string2ex(s2)
+    ex2, ind2, dummy2 = string2ex(s2, arg)
     if length(ind2) > 0
         m1, m2 = extrema(ind2)
-        m1 >= 1 && m2 <= length(arg) || return eval_string(ex1, arg)
+        m1 >= 1 && m2 <= length(arg) || return ex1
     end
-    eval_string(ex2, arg)
+    ex2
 end
 
 translate(p::AbstractString) = translate(p, default_locale_messages())
 
 macro tr_str(p)
-    :(translate($p))
+    #esc(translate(p))
+    ex1, arg, s1 = string2ex(p)
+    arg = Expr(:tuple, arg...)
+    :(eval(translate2($s1, default_locale_messages(), $ex1, $(esc(arg)))))
 end
 
 
